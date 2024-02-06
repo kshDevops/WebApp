cd2545e5d407120fdb87ecd9b2ba6b57aadd723a
sharlink@gmail.com <sharlink@gmail.com>
2024-02-06 22:05:19 +0530

    Add project files.

---
+++ Models/BookAuthor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class BookAuthor
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+        public string? Order { get; set; }
+    }
+}
---
+++ Models/PriceOffer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class PriceOffer
+    {
+        public int PriceOfferId { get; set; }
+        public decimal NewPrice { get; set; }
+        public string? PromotionalText { get; set; }
+        public int BookId { get; set; }
+    }
+}
